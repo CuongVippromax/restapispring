67c55a19f498afd9f65d056b745377d41cd1604b
CuongVippromax <cuongssdz1@gmail.com>
2025-05-29 11:57:07 +0700

    xong phan cors

--- src/main/java/com/cuong/jobhunter/controller/HelloController.java
+++ src/main/java/com/cuong/jobhunter/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.cuong.jobhunter.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/")
+    @CrossOrigin
     public String check() {
         return "check";
     }
